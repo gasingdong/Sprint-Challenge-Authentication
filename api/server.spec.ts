364b79d876e4ff2d053f0275715e216b7101caef
Brandon Dong <gasingdong@gmail.com>
2019-09-20 08:44:08 -0700

    jokes endpoint tests

--- api/server.spec.ts
+++ api/server.spec.ts
@@ -3,16 +3,35 @@ import request from 'supertest';
 import db from '../database/db-config';
 import server from './server';
 
+// Initiate the migrations for the testing environment
 beforeAll(async () => {
   await db.migrate.latest();
 });
 
 describe('server.js', () => {
+  // Delete the table contents before each test
   beforeEach(async () => {
     await db('users').truncate();
   });
 
   it('should set environment to testing', () => {
     expect(process.env.DB_ENV).toBe('testing');
+  });
+
+  describe('jokes endpoint', () => {
+    it('returns 401 FORBIDDEN before logging in', async () => {
+      const res = await request(server).get('/api/jokes');
+      expect(res.status).toBe(401);
+    });
+
+    it('returns 200 OK after logging in', async () => {
+      const { token } = (await request(server)
+        .post('/api/login')
+        .send({ username: 'admin', password: 'admin' })).body;
+      const res = await request(server)
+        .get('/api/jokes')
+        .auth(token, { type: 'bearer' });
+      expect(res.status).toBe(200);
+    });
   });
 });
